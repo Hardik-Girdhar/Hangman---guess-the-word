b3a37d0206540d8fc19aca97c10915b08b931a8f
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-13 09:55:12 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,21 +3,23 @@ import getpass # to generate encrypted word so that other can not see
 lives = 10
 word = getpass.getpass("Player 1 will enter a word to guess: ").lower()
 listword = list(word)
+final = ""
 
 i=0
 print(f"Your main word contain {len(word)} letter")
-hint = "An ___ a day keeps the doctor away."
-print(hint)
 while(i!=len(word)):
     if lives == 0 :
         break
     letter = input("Guess the letter:")
     if(letter == listword[i]):
         print(f"You guessed the write word {letter} now guess next word")
+        final = final + listword[i]
+        print(final)
         i += 1
     else:
         lives -= 1
         print(f"oh.. wrong! You lost your one life left with {lives} lives")
+        print(final)
     
 if lives == 0 :
     print(f"Hey you lost the game😢 the word was {word}")
